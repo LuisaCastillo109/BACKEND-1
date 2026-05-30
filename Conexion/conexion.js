5fdc313ab5a71ce1e647ab2ebe3544cb80a5113b
Luisa Castillo <dkim44243@gmail.com>
2026-05-30 17:08:37 -0500

    modificacion de archivo de conexion.js

--- Conexion/conexion.js
+++ Conexion/conexion.js
@@ -8,20 +8,15 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const db = mysql.createConnection({
-host : process.env.DB_HOST,
-user : process.env.DB_USER,
-password: process.env.DB_PASSWORD,
-database: process.env.DB_NAME,
-port : process.env.DB_PORT || 3306
-});
-
-db.connect((err)=>{
-if (err){
-console.log("DB ERROR COMPLETO:", err)
-return 
-}
-console.log("Conectado a la base de datos")
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT),
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 module.exports = db;
