220066f26d052960f09b521df8cf2482e7be1e91
Luisa Castillo <dkim44243@gmail.com>
2026-05-30 13:14:55 -0500

    modificacion de mysql2

--- Conexion/conexion.js
+++ Conexion/conexion.js
@@ -7,14 +7,20 @@ const app = express();
 app.use (cors());
 app.use (bodyParser.json());
 
-const db = mysql.createConnection(process.env.DATABASE_URL);
+const db = mysql.createConnection({
+host : process.env.DB_HOST,
+user : process.env.DB_USER,
+password: process.env.DB_PASSWORD,
+database: process.env.DB_NAME,
+port : process.env.DB_PORT || 3306
+});
 
-db.connect((err) => {
-  if (err) {
-    console.log("DB ERROR COMPLETO:", err);
-    return;
-  }
-  console.log("Conectado a la base de datos");
+db.connect((err)=>{
+if (err){
+console.log("DB ERROR COMPLETO:", err)
+return 
+}
+console.log("Conectado a la base de datos")
 });
 
 module.exports = db;
