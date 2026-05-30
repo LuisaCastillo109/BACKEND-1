d111ee2fcdbbbcc1cac38ccd81de0c9d790d9ba9
Luisa Castillo <dkim44243@gmail.com>
2026-05-30 12:44:59 -0500

    modificacion de mysql2

--- Conexion/conexion.js
+++ Conexion/conexion.js
@@ -1,4 +1,4 @@
-const mysql = require ("mysql");
+const mysql2 = require ("mysql");
 const cors = require ("cors");
 const express = require ("express");
 const bodyParser = require ("body-parser");
