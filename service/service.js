f2d790241429cd05454885fb0890e756e42e2a68
Luisa Castillo <dkim44243@gmail.com>
2026-05-30 15:30:16 -0500

    modificacion de archivos

--- service/service.js
+++ service/service.js
@@ -1,22 +1,27 @@
-const cors = require("cors");
-const express = require("express");
-const router = require("../Routes/usuario.router");
-const factura = require("../Routes/factura.router");
+const cors = require ("cors");
+const express = require ("express");
+const router = require ("../Routes/usuario.router");
+const factura = require ("../Routes/factura.router")
+const path = require ("path")
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("../service/swagger");
+const PORT = process.env.PORT ||3014
+const app = express();
+const dir = path.join(__dirname, "uploads")
 
 const app = express();
-
-app.use(cors());
-
-// 🔥 ESTO ES LO QUE TE FALTA
+app.use (cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use("/", router);
-app.use("/", factura);
+app.use ("/",router);
+app.use ("/",factura)
 app.use ("/uploads", express.static(dir))
 app.use ("/pdf", express.static(path.join(__dirname,"pdf")))
 app.use("/api-docs",swaggerUi.serve,swaggerUi.setup(swaggerSpec));
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo por el puerto ${PORT}`);
+app.listen(PORT,()=>{
+console.log(`Servidor corriendo por el puerto ${PORT}`)
 });
+
+
+
