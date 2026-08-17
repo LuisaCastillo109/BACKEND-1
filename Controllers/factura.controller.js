c3ddfcb0ca240866a06df3042834f409903b8c74
Luisa Castillo <dkim44243@gmail.com>
2026-08-17 14:29:57 -0500

    modificacion de archivos

--- Controllers/factura.controller.js
+++ Controllers/factura.controller.js
@@ -1,4 +1,3 @@
-require("dotenv").config();
 const db = require("../Conexion/conexion");
 const transporter = require ("../configuracion/configuracion")
 const path = require ("path")
@@ -237,16 +236,12 @@ exports.ObtenerDashboard = (req, res) => {
 };
 
 exports.CrearProducto = async (req, res) => {
-  console.log("🔥🔥🔥 CREAR PRODUCTO FUE LLAMADO 🔥🔥🔥");
+  console.log("🔥🔥🔥 ENTRE A CREAR PRODUCTO 🔥🔥🔥");
 
     console.log(
-        "🔐 BLOB TOKEN EXISTE:",
+        "BLOB TOKEN:",
         !!process.env.BLOB_READ_WRITE_TOKEN
     );
-
-    console.log("📦 FILE:", req.file ? "SÍ LLEGÓ" : "NO LLEGÓ");
-
-    console.log("📩 BODY:", req.body);
     const {
         nombre,
         precio,
